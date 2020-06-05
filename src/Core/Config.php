164c85411089787eadfa5c7977034fc76e6d7046
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-06-05 20:06:25 +0300

    Update Config.php

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -8,6 +8,6 @@ class Config
     const MYSQL_USER_NAME = 'root';
     const MYSQL_PASSWORD = 'root';
     const MYSQL_DATABASE = 'guests_book';
-    const MYSQL_TABLE = 'gb';
+    const MYSQL_TABLE = 'phonebook';
     // const MYSQL_INFO = 'phonebook';
 }
