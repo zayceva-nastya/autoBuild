30364159b7cbcd3329e8d245609bc9fa98aef909
Yury <59015519+YuryS90@users.noreply.github.com>
2020-07-03 21:06:16 +0300

    3.07

--- install/install.php
+++ install/install.php
@@ -5,21 +5,27 @@ require "vendor/autoload.php";
 use TexLab\MyDB\Runner;
 use TexLab\MyDB\DB;
 
-class MyRunner extends Runner
-{
-    protected function errorHandler(array $error)
-    {
-        echo $error["error"] . "\n";
-        echo $error["sql"] . "\n";
-    }
-}
+// class MyRunner extends Runner
+// {
+//     protected function errorHandler(array $error)
+//     {
+//         echo $error["error"] . "\n";
+//         echo $error["sql"] . "\n";
+//     }
+// }
 
-$runner = new MyRunner(DB::Link([
+$runner = new Runner(DB::Link([
     'host' => 'localhost',
     'username' => 'root',
     'password' => 'root',
 ]));
 
 foreach (explode(";", file_get_contents('install/guests_book.sql')) as $value) {
-    $runner->runSQL($value . ";");
+    try {
+        if (!empty($value)) {
+            $runner->runSQL($value);
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
 }
